c435423419e3bd41b3a98da9d96cd1a49655d586
ankur4u007 <ankur4u007@gmail.com>
2015-05-11 23:11:12 +0530

    adding windows installer and context menu options

--- exe_stuff/windowsInstaller/installerWithAdmin.bat
+++ exe_stuff/windowsInstaller/installerWithAdmin.bat
@@ -1,14 +1,5 @@
 @echo off
-
-Regedit /s registrys\reg1.reg
-Regedit /s registrys\reg2.reg
-Regedit /s registrys\reg3.reg
-Regedit /s registrys\reg4.reg
-Regedit /s registrys\reg5.reg
-Regedit /s registrys\reg6.reg
-setx ANKsafeDir "C:\Program Files\ANKsafe"
-
+REG IMPORT %~dp0\ANKsafeReg.reg
 md "C:\Program Files\ANKsafe"
 copy %~dp0\ANKsafe.exe "C:\Program Files\ANKsafe\ANKsafe.exe"
-
 pause
